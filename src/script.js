a2bcecb3c1a6af03e3b9d22573152ebaf27592b3
Bello Ibrahim <ibrahimopeyemibello@gmail.com>
2025-03-31 23:24:18 +0100

    logic updated

--- src/script.js
+++ src/script.js
@@ -4,11 +4,11 @@
 const form = document.querySelector("form");
 
 // select all inputs
-const mortageAmountInput = document.querySelector("mortageAmount");
-const mortageTermYearsInput = document.querySelector("mortageTerm");
-const interestRateInput = document.querySelector("interestRate");
-const repaymentInput = document.querySelector("repayment");
-const interestOnlyInput = document.querySelector("interestOnly");
+const mortageAmountInput = document.querySelector("#mortageAmount");
+const mortageTermYearsInput = document.querySelector("#mortageTerm");
+const interestRateInput = document.querySelector("#interestRate");
+const repaymentInput = document.querySelector("#repayment");
+const interestOnlyInput = document.querySelector("#interestOnly");
 
 // select spans that will have red background if there is error
 const poundsSignErrorSpan = document.querySelector("#poundsSignError");
@@ -17,13 +17,15 @@ const percentageErrorSpan = document.querySelector("#percentageError");
 
 // select all paragraphs that turn red on error
 const amountParagraphRequiredError = document.querySelector(
-  "amountParagraphError"
+  "#amountParagraphError"
 );
-const termParagraphRequiredError = document.querySelector("termParagraphError");
+const termParagraphRequiredError = document.querySelector(
+  "#termParagraphError"
+);
 const interestParagraphRequiredError = document.querySelector(
-  "interestParagraphError"
+  "#interestParagraphError"
 );
-const mortgageTypeRequiredError = document.querySelector("mortgageTypeError");
+const mortgageTypeRequiredError = document.querySelector("#mortgageTypeError");
 
 // select calculate and clear buttons
 const clearAllBtn = document.querySelector("#clearAll");
@@ -33,63 +35,112 @@ const calculateRepaymentsBtn = document.querySelector("#calculateRepayments");
 const emptyResultsDiv = document.querySelector("#emptyResults");
 const resultsShownDiv = document.querySelector("#resultsShown");
 
-form.addEventListener("submit", function (ev) {
-  ev.preventDefault();
-});
+// calculate mortgage
+function calculateMortgage() {}
 
-clearAllBtn.addEventListener("click", function () {
-  form.reset();
-});
-
+// function to validate form and its inputs
 function validateform() {
   let isValid = true;
 
   //   validate mortgage amount input
   if (!mortageAmountInput.value) {
-    amountParagraphRequiredError.classList.remove("hidden");
-    poundsSignErrorSpan.classList.remove("bg-secondary-slate-100");
-    poundsSignErrorSpan.classList.add("error");
+    handleInvalidInput(
+      amountParagraphRequiredError,
+      poundsSignErrorSpan,
+      "This field is required"
+    );
     isValid = false;
   } else if (Number(mortageAmountInput.value) <= 0) {
-    amountParagraphRequiredError.classList.remove("hidden");
-    amountParagraphRequiredError.textContent = "Should be above zero";
-    poundsSignErrorSpan.classList.remove("bg-secondary-slate-100");
-    poundsSignErrorSpan.classList.add("error");
+    handleInvalidInput(
+      amountParagraphRequiredError,
+      poundsSignErrorSpan,
+      "Should be above zero"
+    );
     isValid = false;
+  } else {
+    handleValidInput(amountParagraphRequiredError, poundsSignErrorSpan, "");
+    isValid = true;
   }
 
   //    validate mortgage term input
   if (!mortageTermYearsInput.value) {
-    termParagraphRequiredError.classList.remove("hidden");
-    yearsErrorSpan.classList.remove("bg-secondary-slate-100");
-    yearsErrorSpan.classList.add("error");
+    handleInvalidInput(
+      termParagraphRequiredError,
+      yearsErrorSpan,
+      "This field is required"
+    );
     isValid = false;
   } else if (Number(mortageTermYearsInput.value) <= 0) {
-    termParagraphRequiredError.classList.remove("hidden");
-    termParagraphRequiredError.textContent = "Should be above zero";
-    yearsErrorSpan.classList.remove("bg-secondary-slate-100");
-    yearsErrorSpan.classList.add("error");
+    handleInvalidInput(
+      termParagraphRequiredError,
+      yearsErrorSpan,
+      "Should be above zero"
+    );
     isValid = false;
+  } else {
+    handleValidInput(termParagraphRequiredError, yearsErrorSpan, "");
+    isValid = true;
   }
 
   //   validate interest rate input
   if (!interestRateInput.value) {
-    interestParagraphRequiredError.classList.remove("hidden");
-    percentageErrorSpan.classList.remove("bg-secondary-slate-100");
-    percentageErrorSpan.classList.add("error");
+    handleInvalidInput(
+      interestParagraphRequiredError,
+      percentageErrorSpan,
+      "This field is required"
+    );
     isValid = false;
   } else if (Number(interestRateInput.value) <= 0) {
-    interestParagraphRequiredError.classList.remove("hidden");
-    interestParagraphRequiredError.textContent = "Should be above zero";
-    percentageErrorSpan.classList.remove("bg-secondary-slate-100");
-    percentageErrorSpan.classList.add("error");
+    handleInvalidInput(
+      interestParagraphRequiredError,
+      percentageErrorSpan,
+      "Should be above zero"
+    );
     isValid = false;
+  } else {
+    handleValidInput(interestParagraphRequiredError, percentageErrorSpan, "");
+    isValid = true;
   }
 
   //   validate radio inputs
   if (!repaymentInput.checked && !interestOnlyInput.checked) {
     mortgageTypeRequiredError.classList.remove("hidden");
+    isValid = false;
+  } else {
+    mortgageTypeRequiredError.classList.add("hidden");
+    isValid = true;
   }
 
   return isValid;
 }
+
+// function for to make invalid inuts code DRY
+function handleInvalidInput(errorParagraph, errorSpan, message) {
+  errorParagraph.classList.remove("hidden");
+  errorParagraph.textContent = message;
+  errorSpan.classList.remove("bg-secondary-slate-100");
+  errorSpan.classList.add("error");
+}
+
+// function for to make valid inuts code DRY
+function handleValidInput(errorParagraph, errorSpan, message) {
+  errorParagraph.classList.add("hidden");
+  errorParagraph.textContent = message;
+  errorSpan.classList.add("bg-secondary-slate-100");
+  errorSpan.classList.remove("error");
+}
+
+// add event listener to form and calculate mortage
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  validateform();
+});
+
+// clear all form field
+clearAllBtn.addEventListener("click", function () {
+  form.reset();
+  handleValidInput(amountParagraphRequiredError, poundsSignErrorSpan, "");
+  handleValidInput(termParagraphRequiredError, yearsErrorSpan, "");
+  handleValidInput(interestParagraphRequiredError, percentageErrorSpan, "");
+  mortgageTypeRequiredError.classList.add("hidden");
+});
